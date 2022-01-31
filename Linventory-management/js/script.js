99c6be76505a6b8092aa3697040f11d0de6eabb6
zakaria bijaddiguene <zakariabijaddigune1234@gmail.com>
2022-01-31 18:42:19 +0100

    add script of update and delete buttons

--- Linventory-management/js/script.js
+++ Linventory-management/js/script.js
@@ -33,6 +33,28 @@ taxes.onkeyup = calculPrice
 ads.onkeyup = calculPrice
 discount.onkeyup = calculPrice
 
+//SHOW DATA
+function showData(){
+    title.value=price.value=taxes.value=ads.value=discount.value=total.textContent=category.value = "" 
+    document.querySelector("#productsList").innerHTML = ""
+    for( i = 0 ; i < Allproducts.length ; i++){
+        document.querySelector("#productsList").innerHTML += `
+            <tr>
+                <td>${i+1}</td>
+                <td>${Allproducts[i].title}</td>
+                <td>${Allproducts[i].price}</td>
+                <td>${Allproducts[i].taxes}</td>
+                <td>${Allproducts[i].ads}</td>
+                <td>${Allproducts[i].discount}</td>
+                <td>${Allproducts[i].total}</td>
+                <td>${Allproducts[i].category}</td>
+                <td><button onclick="updateData(${i})" class="table-btn update">update</button></td>
+                <td><button onclick="deleteData(${i})" class="table-btn delete">delete</button></td>
+            </tr>
+        `
+    }
+}
+
 // CREATE 
 document.querySelector(".create").onclick = () => {
     if(title.value != "" && price.value != "" && taxes.value != "" &&  
@@ -40,23 +62,8 @@ document.querySelector(".create").onclick = () => {
         let product = new Product(title.value , price.value , taxes.value , 
             ads.value , discount.value , total.textContent , category.value)
         Allproducts.push(product);
-        document.querySelector("#productsList").innerHTML += `
-            <tr>
-                <td>${Allproducts.length}</td>
-                <td>${product.title}</td>
-                <td>${product.price}</td>
-                <td>${product.taxes}</td>
-                <td>${product.ads}</td>
-                <td>${product.discount}</td>
-                <td>${product.total}</td>
-                <td>${product.category}</td>
-                <td><button class="table-btn update">update</button></td>
-                <td><button class="table-btn delete">delete</button></td>
-            </tr>
-        `
-        title.value=price.value=taxes.value=ads.value=discount.value=total.textContent=category.value = ""
-        
-        
+        showData();
+        console.log(Allproducts)
     }else {
         alert("you should put all values")
     } 
@@ -64,9 +71,36 @@ document.querySelector(".create").onclick = () => {
 
 
 //DELETE
-document.querySelectorAll(".delete").forEach(deleteBtn => {
-    deleteBtn.onclick = ()=>{
-        console.log(deleteBtn.parentElement.parentElement.children[0])
-        deleteBtn.parentElement.parentElement.remove();
-    }
-});
+function deleteData(i){
+    Allproducts.splice(i , 1)
+    showData()
+}
+
+//UPDATE
+let index ; 
+function updateData(i){
+    title.value     = Allproducts[i].title ; 
+    price.value     = Allproducts[i].price ;  
+    taxes.value     = Allproducts[i].taxes ;
+    ads.value       = Allproducts[i].ads ; 
+    discount.value  = Allproducts[i].discount ; 
+    total.innerHTML = Allproducts[i].total ; 
+    category.value  = Allproducts[i].category ; 
+    document.querySelector(".create").style.display = "none"
+    document.querySelector(".updateData").style.display = "block"
+    index = i ;
+    
+}
+function changeData(){
+    Allproducts[index].title = title.value  ; 
+    Allproducts[index].price = price.value ;  
+    Allproducts[index].taxes = taxes.value ;
+    Allproducts[index].ads   = ads.value  ; 
+    Allproducts[index].discount = discount.value  ; 
+    Allproducts[index].total = total.innerHTML ; 
+    Allproducts[index].category = category.value; 
+    document.querySelector(".create").style.display = "block"
+    document.querySelector(".updateData").style.display = "none"
+    showData()
+}
+document.querySelector(".updateData").onclick = changeData
