b0b34833176a9ea344148b52f75b7f494679df3f
zakaria bijaddiguene <zakariabijaddigune1234@gmail.com>
2022-04-17 21:29:08 +0000

    w

--- LandingPage/js/script.js
+++
@@ -1,6 +0,0 @@
-let i = 0 
-document.querySelector(".hamburger").addEventListener("click" , function(){
-    if(i%2 == 0)document.querySelector(".list-items").style.display = "flex"
-    else document.querySelector(".list-items").style.display = "none"
-    i++
-})
