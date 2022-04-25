d12b7d11bb449e7149142951d2fdd0fb38c4b75f
zakaria bijaddiguene <zakariabijaddigune1234@gmail.com>
2022-04-25 17:32:17 +0000

    design

--- static-job-listings-master/script.js
+++ static-job-listings-master/script.js
@@ -8,10 +8,11 @@ async function getData(){
 function setdata(data){
     data.forEach(element => {
         new_featured = ""
-        if(element.new== true) new_featured +="<button class='new'>NEW</button>"
+        if(element.new== true) new_featured +="<button class='new'>NEW!</button>"
         if(element.featured== true) new_featured +="<button class='featured'>FEATURED</button>"
         languages  = ""
         element.languages.forEach(l => languages += "<button>"+l+"</button> " )
+        element.tools.forEach(t => languages += "<button>"+t+"</button> ")
         collection += `
             <div class="employee">
                 <img src=${element.logo}>
@@ -20,6 +21,7 @@ function setdata(data){
                     <h3>${element.position}</h3>
                     <p>${element.postedAt}  -  ${element.contract}  -  ${element.location}</p>
                 </div>
+                <hr>
                 <div class="skills">
                     <button>${element.role}</button>
                     <button>${element.level}</button>
