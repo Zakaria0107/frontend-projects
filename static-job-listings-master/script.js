122c360be05af3bb86b06688a30c7a34cf39a6d4
zakaria bijaddiguene <zakariabijaddigune1234@gmail.com>
2022-04-25 11:37:18 +0000

    new project

---
+++ static-job-listings-master/script.js
@@ -0,0 +1,33 @@
+main = document.querySelector(".main")
+collection = ""
+async function getData(){
+    let responce = await fetch("./data.json")
+    let data = await responce.json()
+    setdata(data)
+}
+function setdata(data){
+    data.forEach(element => {
+        new_featured = ""
+        if(element.new== true) new_featured +="<button class='new'>NEW</button>"
+        if(element.featured== true) new_featured +="<button class='featured'>FEATURED</button>"
+        languages  = ""
+        element.languages.forEach(l => languages += "<button>"+l+"</button> " )
+        collection += `
+            <div class="employee">
+                <img src=${element.logo}>
+                <div class="informations"> 
+                    <p>${element.company} ${new_featured}</p>
+                    <h3>${element.position}</h3>
+                    <p>${element.postedAt}  -  ${element.contract}  -  ${element.location}</p>
+                </div>
+                <div class="skills">
+                    <button>${element.role}</button>
+                    <button>${element.level}</button>
+                    ${languages}
+                </div>
+            </div>
+        `
+    });
+    main.innerHTML = collection
+}
+getData()
