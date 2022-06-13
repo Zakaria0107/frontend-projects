2c07750f8c512c4247d3e6c112a6cd7ecf1b2d65
zakaria bijaddiguene <zakariabijaddigune1234@gmail.com>
2022-06-13 12:51:36 +0100

    completed

---
+++ intro-section-with-dropdown-navigation-main/script.js
@@ -0,0 +1,18 @@
+document.querySelector("#features").onclick = () => {
+    document.querySelector("#features ul ").classList.toggle("hidden");
+}
+document.querySelector("#company").onclick = () => {
+    document.querySelector("#company ul ").classList.toggle("hidden");
+}
+document.querySelector("#mobile-features").onclick = () => {
+    document.querySelector("#mobile-features ul ").classList.toggle("hidden");
+}
+document.querySelector("#mobile-company").onclick = () => {
+    document.querySelector("#mobile-company ul ").classList.toggle("hidden");
+}
+document.querySelector("#menu").onclick = ()=> {
+    document.querySelector("#navBar").classList.toggle("hidden");
+}
+document.querySelector("#close").onclick = ()=> {
+    document.querySelector("#navBar").classList.toggle("hidden");
+}
